9962814e1d39205ed68ac5494f433b211f972a39
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-07-13 20:00:40 +0530

    update

--- backend/controllers/seatController.js
+++ backend/controllers/seatController.js
@@ -60,30 +60,82 @@ const seatController = {
   },
 
   // reserve seats
+
   reserveSeats: async (req, res) => {
     const { numOfSeatsToReserve } = req.body;
 
     if (numOfSeatsToReserve < 1 || numOfSeatsToReserve > 7) {
-      return res.status(400).json({ error: 'Invalid number of seats to reserve' });
+      return res
+        .status(400)
+        .json({ error: "Invalid number of seats to reserve" });
     }
+
+    let allSeats = await Seat.find({}, { isBooked: true, _id: false });
+    let seatsfu = allSeats.map((item) => (item.isBooked == true ? 1 : 0));
+    const totalSeats = await Seat.find().sort("seatNo");
+    let totalLen = Math.floor(Number(totalSeats.length) / 7);
+    let remainLen = Number(totalSeats.length) % 7;
+    let obj = {};
+    for (let i = 0; i < 11; i++) {
+      obj[i] = seatsfu.slice(7 * i + 0, 7 * i + 7);
+    }
+    obj[totalLen] = seatsfu.slice(
+      7 * totalLen,
+      7 * totalLen + Number(remainLen)
+    );
 
     try {
-      const seatsToBook = await Seat.find({ isBooked: false }).sort('seatNo').limit(numOfSeatsToReserve);
+      const seatsToBook = await Seat.find({ isBooked: false })
+        .sort("seatNo")
+        .limit(numOfSeatsToReserve);
 
+      let alreadybooedSeats = await Seat.find({ isBooked: true }).sort(
+        "seatNo"
+      );
       if (seatsToBook.length < numOfSeatsToReserve) {
-        return res.status(400).json({ error: `Only ${seatsToBook.length} seat(s) available` });
+        return res
+          .status(400)
+          .json({ error: `Only ${seatsToBook.length} seat(s) available` });
       }
 
-      for (const seat of seatsToBook) {
-        seat.isBooked = true;
-        await seat.save();
-      }
+      if (alreadybooedSeats.length == 0) {
+        for (const seat of seatsToBook) {
+          seat.isBooked = true;
+          await seat.save();
+        }
+        res.json({ message: "Seats reserved successfully" });
+      } else {
+        let bookseats = numOfSeatsToReserve;
 
-      res.json({ message: 'Seats reserved successfully' });
+        for (let row in obj) {
+          let item = obj[row];
+
+          let available = item.filter((r) => r == 0);
+          console.log(row, totalLen, "checking", bookseats, available.length);
+          if (row == totalLen) {
+            if (Number(bookseats) > Number(available.length)) {
+              throw new Error("limit exceeded");
+              break;
+            }
+          }
+          if (Number(bookseats) <= Number(available.length)) {
+            let rowbooked = item.filter((r) => r == 1);
+
+            for (let j = 0; j < Number(bookseats); j++) {
+              let ot1 = Number(row) * 7 + 1 + j + Number(rowbooked.length) - 1;
+              totalSeats[ot1].isBooked = true;
+              await totalSeats[ot1].save();
+            }
+            res.json({ message: "Seats reserved successfully" });
+            break;
+          }
+        }
+      }
     } catch (error) {
-      res.status(500).json({ error: 'Failed to reserve seats' });
+      res.status(500).json({ error: "Failed to reserve seats" });
     }
   },
+
 };
 
 module.exports = seatController;
